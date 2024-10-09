fb6df36b176361e66f4a404cddae0391a8e8e26a
Fernando <114336753+suferman@users.noreply.github.com>
2024-10-09 09:42:16 +0800

    Create Project_0006 - Crypto_Screener.py

---
+++ Practice_Mini_Projects/Project_0006 - Crypto_Screener.py
@@ -0,0 +1,6 @@
+# Get Data/Prices
+# Volatility calculation function. ATR and/or OBV
+# Traded Value calculation function
+# Moving Average calculation function
+# RSI calculation function
+# ATR calculation function
