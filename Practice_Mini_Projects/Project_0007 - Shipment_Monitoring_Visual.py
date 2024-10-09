8f52eba598995a7fba94eaf40e002efe2419a922
Fernando <114336753+suferman@users.noreply.github.com>
2024-10-09 09:51:50 +0800

    Create Project_0007 - Shipment_Monitoring_Visual.py

---
+++ Practice_Mini_Projects/Project_0007 - Shipment_Monitoring_Visual.py
@@ -0,0 +1,8 @@
+# Get data from excel
+# Track numbers on tracking websites
+# Visualize
+  # Map
+  # Warehouses
+  # Airplanes, color, size based on volume - Green (On-time) or Red (Delayed)
+  # Ships, color, size based on volume - Green (On-time) or Red (Delayed)
+  # Tooltip to show: Description, Volume, ETA, Number of days delayed
